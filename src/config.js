c54a834c7e878fb1ba296fddbb90640d64026cff
Eugene <nitsenko94@gmail.com>
2021-07-14 14:00:15 +0200

    EC-86 fix url

--- src/config.js
+++ src/config.js
@@ -16,7 +16,7 @@ const CONFIG = {
   googleClientId: process.env.googleClientId,
   instagramClientId: process.env.instagramClientId,
   currentUrl,
-  oauthRedirectUrl: `${currentUrl} /login/social/ `,
+  oauthRedirectUrl: `${currentUrl}/login/social/`,
   STRIPE_PUBLISH_KEY: process.env.STRIPE_PUBLISH_KEY,
 };
 
