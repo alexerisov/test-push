5fe16e6e85539694c340dc4c5fb9b2d81e9b8a33
Eugene <nitsenko94@gmail.com>
2021-07-14 14:52:31 +0200

    EC-86 Update redirect for openOAuth modal

--- src/utils/authSocial.js
+++ src/utils/authSocial.js
@@ -1,24 +1,24 @@
 import CONFIG from '@/config.js';
+import {AuthCookieStorage} from '@/utils/web-storage/cookie';
 
 // eslint-disable-next-line max-len
 const params = 'scrollbars=yes,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=500,height=600,left=-1000,top=-1000';
 const USER_TYPE = {
   viewerType: 0,
-  chefType: 1
+  chefType: 1,
 };
 
 const openOAuth = (url, register, user_type) => {
-  // to do: add check for null of user_type
+  // TODO : add check for null of user_type
   const oauthWindow = window.open(url, 'auth', params);
-  let timer = setInterval(function () {
+  let timer = setInterval(function() {
     if (oauthWindow.closed) {
       clearInterval(timer);
-      if (register) {
-        window.location.replace(`${CONFIG.currentUrl}/oauth2-complete/${user_type}`);
+      const {token} = AuthCookieStorage.auth;
+      if (token) {
+        window.location.replace(
+            `${CONFIG.currentUrl}/oauth2-complete/${user_type}`);
       }
-      // else {
-      //   window.location.reload();
-      // }
     }
   }, 1000);
 };
@@ -28,12 +28,13 @@ const openOAuth = (url, register, user_type) => {
  * @param {boolean} register
  * @return {function(): Promise<void>}
  */
-export const loginViaFacebook = (userType = USER_TYPE.viewerType, register = true) => {
+export const loginViaFacebook = (
+    userType = USER_TYPE.viewerType, register = true) => {
   return async () => {
     openOAuth(
-      `https://www.facebook.com/v10.0/dialog/oauth?scope=public_profile email&client_id=${CONFIG.fbClientId}&response_type=token&redirect_uri=${CONFIG.oauthRedirectUrl}&state=${JSON.stringify(
-        { user_type: userType, register: register, backend: 'facebook' })}`,
-      register, userType
+        `https://www.facebook.com/v10.0/dialog/oauth?scope=public_profile email&client_id=${CONFIG.fbClientId}&response_type=token&redirect_uri=${CONFIG.oauthRedirectUrl}&state=${JSON.stringify(
+            {user_type: userType, register: register, backend: 'facebook'})}`,
+        register, userType,
     );
   };
 };
@@ -43,12 +44,17 @@ export const loginViaFacebook = (userType = USER_TYPE.viewerType, register = tru
  * @param {boolean} register
  * @return {function(): Promise<void>}
  */
-export const loginViaGoogle = (userType = USER_TYPE.viewerType, register = true) => {
+export const loginViaGoogle = (
+    userType = USER_TYPE.viewerType, register = true) => {
   return async () => {
     openOAuth(
-      `https://accounts.google.com/o/oauth2/v2/auth?scope=openid email profile&client_id=${CONFIG.googleClientId}&response_type=token&redirect_uri=${CONFIG.oauthRedirectUrl}&state=${JSON.stringify(
-        { user_type: userType, register: register, backend: 'google-oauth2' })}`,
-      register, userType
+        `https://accounts.google.com/o/oauth2/v2/auth?scope=openid email profile&client_id=${CONFIG.googleClientId}&response_type=token&redirect_uri=${CONFIG.oauthRedirectUrl}&state=${JSON.stringify(
+            {
+              user_type: userType,
+              register: register,
+              backend: 'google-oauth2',
+            })}`,
+        register, userType,
     );
   };
 };
@@ -58,12 +64,13 @@ export const loginViaGoogle = (userType = USER_TYPE.viewerType, register = true)
  * @param {boolean} register
  * @return {function(): Promise<void>}
  */
-export const loginViaInstagram = (userType = USER_TYPE.viewerType, register = true) => {
+export const loginViaInstagram = (
+    userType = USER_TYPE.viewerType, register = true) => {
   return async () => {
     openOAuth(
-      `https://api.instagram.com/oauth/authorize?scope=user_profile,user_media&response_type=code&client_id=${CONFIG.instagramClientId}&redirect_uri=${CONFIG.oauthRedirectUrl}&state=${JSON.stringify(
-        { user_type: userType, backend: 'instagram' })}`,
-      register, userType
+        `https://api.instagram.com/oauth/authorize?scope=user_profile,user_media&response_type=code&client_id=${CONFIG.instagramClientId}&redirect_uri=${CONFIG.oauthRedirectUrl}&state=${JSON.stringify(
+            {user_type: userType, backend: 'instagram'})}`,
+        register, userType,
     );
   };
 };
