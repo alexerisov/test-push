fcbc7752be2bd5e85738e796b934931bbbc4a0fb
Oleg Belyaev <olegs.belyaev@yandex.ru>
2021-07-15 14:14:16 +0200

    EC-89 Add rotations for banners

--- src/components/elements/carusel/index.js
+++ src/components/elements/carusel/index.js
@@ -1,74 +1,25 @@
 import React from 'react';
-import classes from "./carusel.module.scss";
+import classes from "./index.module.scss";
 import Recipe from '@/api/Recipe';
 
-const slideWidth = 30;
-
-const sleep = (ms = 0) => {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-};
-
-const CarouselSlideItem = ({pos, idx, activeIdx, itemList}) => {
-
-    const createItem = (position, idx) => {
-      const item = {
-          styles: {
-              transform: `translateX(${position * slideWidth}rem)`,
-          },
-          image: itemList[idx].image,
-      };
-
-      switch (position) {
-          case length - 1:
-              item.styles = {...item.styles, opacity: 0};
-              break;
-          case length + 1:
-              item.styles = {...item.styles, opacity: 0};
-              break;
-          case length:
-              break;
-          default:
-              item.styles = {...item.styles};
-              break;
-      }
-
-      return item;
-    };
-    const item = createItem(pos, idx, activeIdx);
-
-    return (
-        <li className={classes.carousel__slideItem} style={item.styles}>
-            <div className={classes.carousel__slideItemImgLink}>
-                <img src={item.image} alt='Banner' />
-            </div>
-        </li>
-    );
-};
-
 const Carousel = () => {
+    const FIRST_POSITION = 0;
     const [itemList, setItemList] = React.useState([]);
-    const [items, setItems] = React.useState([]);
-    const [isTicking, setIsTicking] = React.useState(false);
-    const [activeIdx, setActiveIdx] = React.useState(0);
-    const bigLength = items.length;
+    const [position, setPosition] = React.useState(FIRST_POSITION);
 
-    const prevClick = (jump = 1) => {
-        if (!isTicking) {
-            setIsTicking(true);
-            setItems((prev) => {
-                return prev.map((_, i) => prev[(i + jump) % bigLength]);
-            });
+    const prevClick = () => {
+        if (position === FIRST_POSITION ) {
+            setPosition(itemList.length - 1);
+        } else {
+            setPosition(position - 1);
         }
     };
 
-    const nextClick = (jump = 1) => {
-        if (!isTicking) {
-            setIsTicking(true);
-            setItems((prev) => {
-                return prev.map(
-                    (_, i) => prev[(i - jump + bigLength) % bigLength],
-                );
-            });
+    const nextClick = () => {
+        if (position === (itemList.length - 1)) {
+            setPosition(FIRST_POSITION);
+        } else {
+            setPosition(position + 1);
         }
     };
 
@@ -80,36 +31,36 @@ const Carousel = () => {
     }, []);
 
     React.useEffect(() => {
-      setItems(Array.from(Array(itemList.length).keys()));
-    }, [itemList]);
+        const timer = setInterval(() => {
+            nextClick();
+        }, 2000);
 
-    React.useEffect(() => {
-        if (isTicking) sleep(300).then(() => setIsTicking(false));
-    }, [isTicking]);
+        return () => clearInterval(timer);
+    });
 
     return (
         <div className={classes.carousel__wrap}>
-            <div className={classes.carousel__inner}>
-                <button className={classes.carousel__btn__prev} onClick={() => prevClick()}>
-                    <i className={classes.carousel__btn_arrow__left} />
-                </button>
-                <div className={classes.carousel__container}>
-                    <ul className={classes.carousel__slide_list}>
-                        {items.map((pos, i) => (
-                            <CarouselSlideItem
-                                key={i}
-                                idx={i}
-                                pos={pos}
-                                activeIdx={activeIdx}
-                                itemList={itemList}
-                            />
-                        ))}
-                    </ul>
-                </div>
-                <button className={classes.carousel__btn__next} onClick={() => nextClick()}>
-                    <i className={classes.carousel__btn_arrow__right} />
-                </button>
+            <button className={classes.carousel__btn__prev} onClick={() => prevClick()}>
+                <i className={classes.carousel__btn_arrow__left} />
+            </button>
+            <div className={classes.carousel__container}>
+                {
+                    itemList.map((item, index) => {
+                        return  <div
+                                    key={`banner-${index}`}
+                                    className={classes.carousel__banner}
+                                    style={{
+                                        backgroundImage: `url(${item.image})`,
+                                        display: `${index == position ? 'block' : 'none'}`,
+                                    }}
+                                >
+                                </div>;
+                    })
+                }
             </div>
+            <button className={classes.carousel__btn__next} onClick={() => nextClick()}>
+                <i className={classes.carousel__btn_arrow__right} />
+            </button>
         </div>
     );
 };
