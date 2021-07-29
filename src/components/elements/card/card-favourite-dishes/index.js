0397943e06340eb666550eacfa6685a946448d7b
Dmitriev R.R <education.romichdmitriev@gmail.com>
2021-07-29 12:54:15 +0500

    fix: [EC-126] add link to recipe for card-favourite-dishes

--- src/components/elements/card/card-favourite-dishes/index.js
+++ src/components/elements/card/card-favourite-dishes/index.js
@@ -2,22 +2,32 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
+import { CardActionArea } from '@material-ui/core';
+import {useRouter} from "next/router";
 
 import classes from "./index.module.scss";
 
-const CardFavouriteDishes = ({image, title}) => {
+const CardFavouriteDishes = ({image, title, recipeId}) => {
+  const router = useRouter();
+
+  const redirectToRecipeCard = (id) => {
+    router.push(`/recipe/${id}`);
+  };
+
   return (
     <Card className={classes.card}>
-      <CardMedia
-        className={classes.card__media}
-        image={image}
-        title=""
-      />
-      <CardContent className={classes.card__content}>
-        <div>
-          <p className={classes.card__title} title={title}>{title}</p>
-        </div>
-      </CardContent>
+      <CardActionArea onClick={() => redirectToRecipeCard(recipeId)}>
+        <CardMedia
+          className={classes.card__media}
+          title=""
+          image={image}
+        />
+        <CardContent className={classes.card__content}>
+          <div>
+            <p className={classes.card__title}>{title}</p>
+          </div>
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 };
