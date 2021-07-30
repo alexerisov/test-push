b86e85a32033e383059d5538e70e7bf1463ce14c
Dmitriev R.R <education.romichdmitriev@gmail.com>
2021-07-30 15:49:04 +0500

    feat: [EC-167] set Text Limits

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,7 @@
 // User types
 export const CHEF_TYPE = 1;
 export const VIEWER_TYPE = 0;
+
+// Validate TextField
+export const MAX_LENGTH_LIMIT_ACHIEVED = 'You have achieved max of';
+export const REQUIRED_FIELD = 'Required field';
