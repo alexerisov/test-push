ef3c37973bee7b45f364721b61afe80607888f5a
Svetlana Khaustova <svetlanka.ender@yandex.ru>
2021-08-03 06:41:01 +0200

    EC-116 Add verification email and 404 page

---
+++ src/pages/404/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "next/link";
+import classes from "./index.module.scss";
+import LayoutPage from '@/components/layouts/layout-page';
+
+const Custom404 = () => {
+
+  const content = (
+    <div className={classes.notFound}>
+      <h1>Page not found</h1>
+      <Link href="/"><a>Home</a></Link>
+    </div>
+  );
+
+  return <LayoutPage content={content} />;
+};
+
+export default Custom404;
