cc1b58c18a79cf3aa65762982df8e6376acefd10
Svetlana Khaustova <svetlanka.ender@yandex.ru>
2021-08-03 07:15:47 +0200

    EC-171 Make cards compact

--- src/components/elements/card-highest-meals/index.js
+++ src/components/elements/card-highest-meals/index.js
@@ -18,6 +18,18 @@ const StyledCardMedia = styled(CardMedia)`
   }
 `;
 
+const StyledCardContent = styled(CardContent)`
+  padding: 16px;
+`;
+
+const StyledCardActionArea = styled(CardActionArea)`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: flex-start;
+`;
+
 const CardHighestMeals = (props) => {
 
   const router = useRouter();
@@ -45,13 +57,13 @@ const CardHighestMeals = (props) => {
 
   return (
     <Card className={classes.card}>
-      <CardActionArea onClick={() => redirectToRecipeCard(props.id)}>
+      <StyledCardActionArea onClick={() => redirectToRecipeCard(props.id)}>
         <StyledCardMedia
           className={classes.card__media}
           image={props.image}
           title=""
         />
-        <CardContent className={classes.card__content}>
+        <StyledCardContent className={classes.card__content}>
           <div>
             <p className={classes.card__name} title={props.title}>{props.title}</p>
             <p className={classes.card__author}>{`by Chef ${props.name}`}</p>
@@ -61,8 +73,8 @@ const CardHighestMeals = (props) => {
             {props.publishStatus &&
             <div className={classes.card__status}>{getStatusOfCard()}</div>}
           </div>
-        </CardContent>
-      </CardActionArea>
+        </StyledCardContent>
+      </StyledCardActionArea>
     </Card>
   );
 };
