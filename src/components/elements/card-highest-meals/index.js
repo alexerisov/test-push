bfa8efe880b634a827766d431a6a7834463b0d7c
Dmitriev R.R <education.romichdmitriev@gmail.com>
2021-08-11 21:01:19 +0500

    refactor: [EC-134] reformat with Prettier

--- src/components/elements/card-highest-meals/index.js
+++ src/components/elements/card-highest-meals/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import classes from "./index.module.scss";
+import classes from './index.module.scss';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import RaitingIcon from "@/components/elements/rating-icon";
-import LikeIcon from "@/components/elements/like-icon";
-import Link from "next/link";
+import RaitingIcon from '@/components/elements/rating-icon';
+import LikeIcon from '@/components/elements/like-icon';
 import styled from 'styled-components';
 import { CardActionArea } from '@material-ui/core';
 import { useRouter } from 'next/router';
 
-import { PUBLISH_STATUS, APPROVED_STATUS } from "@/utils/datasets";
+import { PUBLISH_STATUS, APPROVED_STATUS } from '@/utils/datasets';
 
 const StyledCardMedia = styled(CardMedia)`
   .MuiCardMedia-root {
@@ -30,10 +29,10 @@ const StyledCardActionArea = styled(CardActionArea)`
   justify-content: flex-start;
 `;
 
-const CardHighestMeals = (props) => {
+const CardHighestMeals = props => {
   const router = useRouter();
 
-  const redirectToRecipeCard = (id) => {
+  const redirectToRecipeCard = id => {
     router.push(`/recipe/${id}`);
   };
 
@@ -57,17 +56,17 @@ const CardHighestMeals = (props) => {
   return (
     <Card className={classes.card}>
       <StyledCardActionArea onClick={() => redirectToRecipeCard(props.id)}>
-        <StyledCardMedia
-          className={classes.card__media}
-          image={props.image}
-          title=""
-        />
+        <StyledCardMedia className={classes.card__media} image={props.image} title="" />
         <StyledCardContent className={classes.card__content}>
           <div>
-            <p className={classes.card__name} title={props.title}>{props.title}</p>
+            <p className={classes.card__name} title={props.title}>
+              {props.title}
+            </p>
             <p className={classes.card__author}>{`by Chef ${props.name}`}</p>
             <p className={classes.card__location}>{props.city}</p>
-            <div className={classes.card__likeIcon}><LikeIcon value={props.likes} /></div>
+            <div className={classes.card__likeIcon}>
+              <LikeIcon value={props.likes} />
+            </div>
             {props.publishStatus &&
             <div className={classes.card__status}>{getStatusOfCard()}</div>}
           </div>
@@ -76,5 +75,5 @@ const CardHighestMeals = (props) => {
     </Card>
   );
 };
-  
+
 export default CardHighestMeals;
