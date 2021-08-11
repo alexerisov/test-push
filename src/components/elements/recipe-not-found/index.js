9c372fd5d6d251404588bf89f5da369817c5d691
Dmitriev R.R <education.romichdmitriev@gmail.com>
2021-08-11 19:34:52 +0500

    refactor: [EC-262] reformat with prettier

--- src/components/elements/recipe-not-found/index.js
+++ src/components/elements/recipe-not-found/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import classes from "./index.module.scss";
-import Link from "next/link";
+import classes from './index.module.scss';
+import Link from 'next/link';
 
 const RecipeNotFound = () => {
-    return (
-        <div className={classes.notFound}>
-            <h1 className={classes.notFound__title}>Recipe not found</h1>
-            <Link href="/"><a>Home</a></Link>
-        </div>
-    );
+  return (
+    <div className={classes.notFound}>
+      <h1 className={classes.notFound__title}>Recipe not found</h1>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </div>
+  );
 };
 
 export default RecipeNotFound;
