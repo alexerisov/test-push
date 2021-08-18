0a574a7118c0660f245db2b41e9cf47e01730280
Svetlana Khaustova <svetlanka.ender@yandex.ru>
2021-08-18 17:32:52 +0200

    EC-297

--- src/pages/confirm/email/[code].js
+++ src/pages/confirm/email/[code].js
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { connect } from "react-redux";
-import { LayoutPage } from "@/components/layouts";
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { LayoutPage } from '@/components/layouts';
 import { loginActions } from '@/store/actions';
 import { useRouter } from 'next/router';
-import HeaderDefault from "@/components/elements/header-default";
-import classes from "./index.module.scss";
+import HeaderDefault from '@/components/elements/header-default';
+import classes from './index.module.scss';
 import Link from 'next/link';
-import { modalActions } from '@/store/actions';
 
-const ConfirmEmail = (props) => {
+const ConfirmEmail = props => {
   const router = useRouter();
   const [response, setResponse] = useState();
   const [code, setCode] = useState();
@@ -19,37 +18,32 @@ const ConfirmEmail = (props) => {
 
   useEffect(() => {
     if (code) {
-      props.dispatch(
-        loginActions.loginWithLink(code),
-      ).then((res) => {
-        setResponse(res);
-      }).catch(e => {
-        router.push("/404");
-      });
+      props
+        .dispatch(loginActions.loginWithLink(code))
+        .then(res => {
+          setResponse(res);
+        })
+        .catch(e => {
+          router.push('/404');
+        });
     }
   }, [code]);
 
-  const handleClickLogin = (name) => {
-    return () => {
-      props.dispatch(
-        modalActions.open(name),
-      ).then(result => {
-        // result when modal return promise and close
-      });
-    };
-  };
-
   const content = (
     <>
-      {!response ? <h1>Loading...</h1> :
+      {!response ? (
+        <h1>Loading...</h1>
+      ) : (
         <div className={classes.confirm}>
           <div>
-            <h2 className={classes.confirm__successTitle}>EMAIL SUCCESSFUL</h2>
-            <p className={classes.confirm__successSubTitle} onClick={props.onClose}>Let us know about your interests</p>
+            <h2 className={classes.confirm__successTitle}>Email verification is successful</h2>
+            <p className={classes.confirm__successSubTitle} onClick={props.onClose}>
+              Let us know about your interests
+            </p>
             <p className={classes.confirm__successText} onClick={props.onClose}>
-              <button className={classes.confirm__buttonLogin} onClick={handleClickLogin('register')}>
-                Login
-              </button>
+              <Link href="/profile/account-settings">
+                <a className={classes.confirm__link}>Go to profile settings </a>
+              </Link>
               or continue
               <Link href="/search">
                 <a className={classes.confirm__link}> browsing</a>
@@ -57,7 +51,7 @@ const ConfirmEmail = (props) => {
             </p>
           </div>
         </div>
-      }
+      )}
     </>
   );
   return <LayoutPage content={content} header={<HeaderDefault />} />;
