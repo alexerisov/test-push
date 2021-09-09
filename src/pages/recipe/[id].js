7256e0340ad183129db256f5c4b7a2e6c6409a7f
Dmitriev R.R <education.romichdmitriev@gmail.com>
2021-09-09 11:47:32 +0500

    fix: [EC-287] fix props in recipe comments

--- src/pages/recipe/[id].js
+++ src/pages/recipe/[id].js
@@ -473,7 +473,7 @@ function RecipePage(props) {
                   })}
                 </div>
               )}
-              <ResipeComments recipeId={recipeId} userId={userId}/>
+              <ResipeComments id={recipeId} userId={userId}/>
             </div>
             <div className={classes.recipe__cards}>
               {latestRecipes && (
