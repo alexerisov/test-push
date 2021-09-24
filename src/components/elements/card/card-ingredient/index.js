e2557b056920164e99bed41b35a131a6d0458fc4
Svetlana Khaustova <svetlanka.ender@yandex.ru>
2021-09-24 08:03:40 +0200

    EC-397 Add new style cards ingredient

--- src/components/elements/card/card-ingredient/index.js
+++ src/components/elements/card/card-ingredient/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import classes from "./card-ingredient.module.scss";
+import classes from './card-ingredient.module.scss';
 
-const CardIngredient = (props) => {
-  const {title, quantity, unit, id} = props;
+const CardIngredient = props => {
+  const { title, quantity, unit, id } = props;
   const handleDelete = () => {
     props.delete(id);
   };
@@ -10,14 +10,17 @@ const CardIngredient = (props) => {
   return (
     <div className={classes.cardIngredients}>
       <h2 className={classes.cardIngredients__title}>{title}</h2>
-      <p className={classes.cardIngredients__text}>{quantity} {unit}</p>
-      <button
-        type="button"
-        className={classes.cardIngredients__button}
-        onClick={handleDelete}>
+      <p className={classes.cardIngredients__text}>
+        {quantity} {unit}
+      </p>
+      <button type="button" className={classes.cardIngredients__button} onClick={handleDelete}>
+        <div>
+          <div className={classes.cardIngredients__button__line}></div>
+          <div className={classes.cardIngredients__button__line}></div>
+        </div>
       </button>
     </div>
   );
 };
-  
+
 export default CardIngredient;
