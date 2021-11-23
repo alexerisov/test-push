efdde5435f32d0080634c62c4279b64f8aecbba5
Alexey Erisov <alexerisov@gmail.com>
2021-11-23 12:58:06 +0700

    fix: Try to fix styles

--- src/pages/order-confirm/index.jsx
+++ src/pages/order-confirm/index.jsx
@@ -8,7 +8,7 @@ import { withAuth } from '@/utils/authProvider';
 import { BasicInput } from '@/components/basic-elements/basic-input';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
-import { Button, NoSsr } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import { InputsBlock } from '@/components/basic-blocks/inputs-block';
 
 const useStyles = makeStyles(theme => ({
@@ -23,16 +23,17 @@ const useStyles = makeStyles(theme => ({
     margin: '48px 0px'
   },
   button: {
-    background: '#FFAA00',
+    background: '#FFAA00 !important',
     borderRadius: '90px !important',
-    margin: '32px 0px',
-    fontSize: '16px',
-    lineHeight: '160%',
+    margin: '32px 0px  !important',
+    fontWeight: 'bold !important',
+    fontSize: '16px !important',
+    lineHeight: '160% !important',
     textAlign: 'center',
     letterSpacing: '0.01em',
-    color: '#FCFCFD',
+    color: '#FCFCFD !important',
     '&:hover': {
-      backgroundColor: '#FB8C00'
+      backgroundColor: '#FB8C00 !important'
     }
   },
   page__title: {
@@ -54,11 +55,17 @@ const useStyles = makeStyles(theme => ({
   [theme.breakpoints.only('sm')]: {
     content: {
       gridTemplateColumns: '385 400px'
+    },
+    col2: {
+      order: -1
     }
   },
   [theme.breakpoints.only('xs')]: {
     content: {
       gridTemplateColumns: '1fr'
+    },
+    col2: {
+      order: -1
     }
   }
 }));
@@ -66,7 +73,11 @@ const useStyles = makeStyles(theme => ({
 const validationSchema = yup.object({
   email: yup.string('Enter your email').email('Enter a valid email').required('Email is required'),
   name: yup.string('Enter your name').required('Name is required'),
-  phone: yup.string('Enter your phone').required('Phone number is required')
+  phone: yup.string('Enter your phone').required('Phone number is required'),
+  city: yup.string('Enter your city').required('City is required'),
+  street: yup.string('Enter your street').required('Street is required'),
+  house: yup.string('Enter your house').required('House is required'),
+  flat: yup.string('Enter your flat').required('Flat is required')
 });
 
 const OrderConfirmPage = () => {
@@ -79,8 +90,6 @@ const OrderConfirmPage = () => {
       setTotal(localStorage.getItem('total'));
     }
   });
-
-  console.log('fromstorage', products);
 
   const formik = useFormik({
     initialValues: {
@@ -101,49 +110,50 @@ const OrderConfirmPage = () => {
 
   let content = (
     <div className={styles.content}>
-      <NoSsr>
-        <div className="col1">
-          <div className={styles.page__title}>Confirm and Pay</div>
+      <div className="col1">
+        <div className={styles.page__title}>Confirm and Pay</div>
+        <hr className={styles.divider} />
+        <form onSubmit={formik.handleSubmit}>
+          <BasicInput formik={formik} label="Email" name="email" placeholder="youremail@gmail.net" />
+          <InputsBlock title="Your details">
+            <BasicInput formik={formik} label="Email" name="email" placeholder="youremail@gmail.net" />
+            <BasicInput formik={formik} size={0.5} label="Name" name="name" placeholder="Enter your name" />
+            <BasicInput formik={formik} size={0.5} label="Phone" name="phone" placeholder="Phone number" />
+          </InputsBlock>
           <hr className={styles.divider} />
-          <form onSubmit={formik.handleSubmit}>
-            <InputsBlock title="Your details">
-              <BasicInput formik={formik} label="Email" name="email" placeholder="youremail@gmail.net" />
-              <BasicInput size={0.5} formik={formik} label="Name" name="name" placeholder="Enter your name" />
-              <BasicInput size={0.5} formik={formik} label="Phone" name="phone" placeholder="Phone number" />
-            </InputsBlock>
-            <hr className={styles.divider} />
-            <InputsBlock title="Shipping">
-              <InputsBlock.Tabs>
-                <InputsBlock.Tab label="courier" />
-                <InputsBlock.Tab label="self-delivery" />
-              </InputsBlock.Tabs>
-              <InputsBlock.TabPanel index={0}>
-                <BasicInput size={0.5} formik={formik} label="City" name="city" placeholder="Enter your city" />
-                <BasicInput size={0.5} formik={formik} label="Street" name="street" placeholder="Enter your street" />
-                <BasicInput size={0.5} formik={formik} label="House" name="house" placeholder="Enter your house" />
-                <BasicInput size={0.5} formik={formik} label="Flat" name="flat" placeholder="Enter your flat" />
-              </InputsBlock.TabPanel>
-              <InputsBlock.TabPanel index={1}>
-                <div>self-delivery</div>
-              </InputsBlock.TabPanel>
-            </InputsBlock>
-            <hr className={styles.divider} />
-            <InputsBlock title="Pay with">
-              <InputsBlock.Tabs>
-                <InputsBlock.Tab label="Paypal" />
-                <InputsBlock.Tab label="Credit card" />
-              </InputsBlock.Tabs>
-              <InputsBlock.TabPanel index={0}>
-                <div>Pay Pal</div>
-              </InputsBlock.TabPanel>
-              <InputsBlock.TabPanel index={1}>
-                <div>Credit card</div>
-              </InputsBlock.TabPanel>
-            </InputsBlock>
-            <Button className={styles.button}>Confirm and Pay</Button>
-          </form>
-        </div>
-      </NoSsr>
+          <InputsBlock title="Shipping">
+            <InputsBlock.Tabs>
+              <InputsBlock.Tab label="courier" />
+              <InputsBlock.Tab label="self-delivery" />
+            </InputsBlock.Tabs>
+            <InputsBlock.TabPanel index={0}>
+              <BasicInput formik={formik} size={0.5} label="City" name="city" placeholder="Enter your city" />
+              <BasicInput formik={formik} size={0.5} label="Street" name="street" placeholder="Enter your street" />
+              <BasicInput formik={formik} size={0.5} label="House" name="house" placeholder="Enter your house" />
+              <BasicInput formik={formik} size={0.5} label="Flat" name="flat" placeholder="Enter your flat" />
+            </InputsBlock.TabPanel>
+            <InputsBlock.TabPanel index={1}>
+              <div>self-delivery</div>
+            </InputsBlock.TabPanel>
+          </InputsBlock>
+          <hr className={styles.divider} />
+          <InputsBlock title="Pay with">
+            <InputsBlock.Tabs>
+              <InputsBlock.Tab label="Paypal" />
+              <InputsBlock.Tab label="Credit card" />
+            </InputsBlock.Tabs>
+            <InputsBlock.TabPanel index={0}>
+              <div>Pay Pal</div>
+            </InputsBlock.TabPanel>
+            <InputsBlock.TabPanel index={1}>
+              <div>Credit card</div>
+            </InputsBlock.TabPanel>
+          </InputsBlock>
+          <Button type="submit" className={styles.button}>
+            Confirm and Pay
+          </Button>
+        </form>
+      </div>
       <div className={styles.col2}>
         <Basket products={products} total={total} />
       </div>
