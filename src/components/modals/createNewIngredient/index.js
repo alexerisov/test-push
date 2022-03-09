2c6d2f166e4aae51da8cd4d7e150ea04a85e149b
Alexey Erisov <alexerisov@gmail.com>
2022-03-09 18:23:37 +0700

    upd: Rework add ingredient modal

--- src/components/modals/createNewIngredient/index.js
+++ src/components/modals/createNewIngredient/index.js
@@ -3,10 +3,10 @@ import { makeStyles } from '@material-ui/core/styles';
 import { LayoutModal } from '@/components/layouts';
 import { modalActions, recipeUploadActions } from '@/store/actions';
 import { connect } from 'react-redux';
-import classes from './addIngredient.module.scss';
+import classes from './index.module.scss';
 import TextField from '@material-ui/core/TextField';
 import { units } from '@/utils/datasets';
-import { Select, MenuItem, Collapse, Dialog } from '@material-ui/core';
+import { Select, MenuItem, Collapse } from '@material-ui/core';
 import { getNumberWithMaxDigits } from '@/utils/helpers';
 import { BasicIcon } from '@/components/basic-elements/basic-icon';
 import { ReactComponent as CloseIcon } from '../../../../public/icons/Close Circle/Line.svg';
@@ -18,7 +18,6 @@ import http from '@/utils/http';
 import ErrorBoundary from '@/components/basic-blocks/error-boundary';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
-import Recipe from '@/api/Recipe';
 
 const useStyles = makeStyles(theme => ({
   textField: {
@@ -58,18 +57,14 @@ const fetcher = (...args) =>
     });
 
 const validationSchema = yup.object({
-  basicIngredient: yup.string().required('Ingredient is required'),
+  basiIngredient: yup.string().required('Ingredient is required'),
   quantity: yup
     .number()
     .min(0, 'Quantity should be greater than 0')
     .max(99999, 'Quantity should be less than 99999')
     .required('Quantity is required'),
-  unit: yup.string().required('Unit is required')
-});
-
-const validationSchema2 = yup.object({
-  name: yup.string().required('Name is required'),
-  group: yup.string().required('Group is required')
+  unit: yup.string().required('Unit is required'),
+  group: yup.string()
 });
 
 const defaultUnits = () => {
@@ -97,27 +92,22 @@ const renderOption = (option, { selected }) => {
   );
 };
 
-function AddIngredient(props) {
+function CreateNewIngredient(props) {
   const classMarerialUi = useStyles();
   const { data } = props.recipeUpload;
   const [basicIngredient, setBasicIngredient] = useState(null);
   const [shouldLoadUnits, setShouldLoadUnits] = useState();
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   const formik = useFormik({
     initialValues: {
-      basicIngredient: '',
       customTitle: '',
       quantity: '',
       unit: '',
       old_unit: '',
-      createIngredient: {
-        name: '',
-        group: ''
-      }
+      basicIngredient: ''
     },
     validateOnBlur: true,
-    validateOnChange: true,
+    validateOnChang: true,
     validationSchema: validationSchema,
     onSubmit: values => {
       const preparedValues = {
@@ -141,31 +131,6 @@ function AddIngredient(props) {
     }
   });
 
-  const formik2 = useFormik({
-    initialValues: {
-      name: '',
-      group: ''
-    },
-    validateOnBlur: true,
-    validateOnChange: true,
-    validationSchema: validationSchema2,
-    onSubmit: async values => {
-      try {
-        const response = await Recipe.createBasicIngredient({ title: values.name, group: values.group });
-        setBasicIngredient(response.data);
-        formik.setFieldValue('basicIngredient', response.data.title);
-        handleClose();
-      } catch (e) {
-        if (e.response.data?.title) {
-          formik2.setFieldError('name', e.response.data?.title);
-        }
-        if (e.response.data?.group) {
-          formik2.setFieldError('group', e.response.data?.title);
-        }
-      }
-    }
-  });
-
   const {
     data: autocompleteSuggestions,
     error: autocompleteError,
@@ -182,11 +147,6 @@ function AddIngredient(props) {
     error: basicIngredientUnitError,
     isValidating: isBasicIngredientUnitLoading
   } = useSWR(shouldLoadUnits ? `/recipe/units/${basicIngredient?.pk}` : null, fetcher);
-
-  const { data: ingredientGroups, error: ingredientGroupsError } = useSWR(
-    isDialogOpen ? `/recipe/ingredient_groups` : null,
-    fetcher
-  );
 
   const onCancel = () => {
     props.dispatch(modalActions.close());
@@ -217,7 +177,6 @@ function AddIngredient(props) {
     } else {
       formik.setFieldValue('basicIngredient', '');
     }
-    formik.validateField('basicIngredient');
   };
 
   const handleSelectBasicIngredient = (event, newValue) => {
@@ -257,23 +216,10 @@ function AddIngredient(props) {
     return result;
   };
 
-  const handleClickOpen = () => {
-    formik2.setFieldValue('name', formik.values.basicIngredient, false);
-    setIsDialogOpen(true);
-  };
-
-  const handleClose = () => {
-    formik2.resetForm();
-    setIsDialogOpen(false);
-  };
-
-  const handleSelectGroup = event => {
-    formik2.setFieldValue('group', event.target.value);
-  };
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    formik2.handleSubmit(event);
+  const handleClickPopupOpen = (name, params) => {
+    return () => {
+      props.dispatch(modalActions.open(name, params));
+    };
   };
 
   const renderContent = () => {
@@ -281,53 +227,6 @@ function AddIngredient(props) {
       <div className={classes.addIngredient}>
         <h2 className={classes.addIngredient__title}>Add More Ingredients</h2>
         <form className={classes.addIngredient__form} onSubmit={formik.submitForm}>
-          <ErrorBoundary>
-            <label htmlFor="addIngredient-group" className={classes.addIngredient__label}>
-              Ingredient
-            </label>
-            <Autocomplete
-              classes={{
-                endAdornment: classes.addIngredient__close_icon
-              }}
-              key="autocomplete-basicIngredient"
-              fullWidth
-              id="autocomplete-basicIngredient"
-              options={getOptionsList()}
-              freeSolo
-              autoComplete
-              includeInputInList
-              autoHighlight
-              filterOptions={filterOptions}
-              getOptionLabel={option => option.title}
-              onChange={(event, newValue) => {
-                if (newValue?.title === 'empty') {
-                  handleClickOpen();
-                  formik.setFieldValue('basicIngredient', '');
-                }
-                formik.setFieldValue('basicIngredient', newValue?.title || '');
-              }}
-              inputValue={formik.values.basicIngredient}
-              onBlur={onBlur}
-              onInputChange={(_, newValue) => onChangeBasicIngredient({ target: { value: newValue } })}
-              loading={isAutocompleteLoading}
-              renderOption={renderOption}
-              closeIcon={<BasicIcon icon={CloseIcon} color="#B1B5C3" size="32px" />}
-              renderInput={params => (
-                <TextField
-                  {...params}
-                  id="addIngredient-basicIngredient"
-                  name="basicIngredient"
-                  variant="outlined"
-                  fullWidth
-                  placeholder="Please select ingredient"
-                  className={classMarerialUi.textField}
-                  onChange={onChangeBasicIngredient}
-                  value={formik.values.basicIngredient}
-                />
-              )}
-            />
-          </ErrorBoundary>
-
           <div>
             <label htmlFor="addIngredient-title" className={classes.addIngredient__label}>
               Extra information
@@ -343,21 +242,7 @@ function AddIngredient(props) {
               value={formik.values.customTitle}
             />
           </div>
-
-          <div className={classes.addIngredient__container}>
-            <label htmlFor="addIngredient-quantity" className={classes.addIngredient__label}>
-              Quantity
-            </label>
-            <TextField
-              id="addIngredient-quantity"
-              name="quantity"
-              type="number"
-              value={formik.values.quantity}
-              onChange={formik.handleChange}
-              variant="outlined"
-              fullWidth
-              className={classMarerialUi.textField}
-            />
+          <div>
             <label htmlFor="create-types-select" className={classes.addIngredient__label}>
               Unit
             </label>
@@ -365,7 +250,6 @@ function AddIngredient(props) {
               MenuProps={MenuProps}
               id="addIngredient-unit"
               name="unit"
-              disabled={!basicIngredient?.pk}
               value={formik.values.unit}
               onChange={handleSelectUnit}
               variant="outlined"
@@ -392,56 +276,6 @@ function AddIngredient(props) {
             {Object.values(formik.errors)?.length > 0 && <p>{Object.values(formik.errors)[0]}</p>}
           </div>
         </form>
-        <Dialog open={isDialogOpen} aria-labelledby="form-dialog-title">
-          <LayoutModal onClose={handleClose} themeName="white_small">
-            <div className={classes.addIngredient}>
-              <h2 className={classes.addIngredient__title}>Create new ingredient</h2>
-              <form className={classes.addIngredient__form} onSubmit={handleSubmit}>
-                <div>
-                  <label htmlFor="addIngredient-title" className={classes.addIngredient__label}>
-                    Name
-                  </label>
-                  <TextField
-                    id="createIngredient-title"
-                    name="name"
-                    variant="outlined"
-                    fullWidth
-                    placeholder="Enter ingredient name"
-                    className={classMarerialUi.textField}
-                    onChange={formik2.handleChange}
-                    value={formik2.values.name}
-                  />
-                </div>
-                <div>
-                  <label htmlFor="create-types-select" className={classes.addIngredient__label}>
-                    Group
-                  </label>
-                  <Select
-                    MenuProps={MenuProps}
-                    id="createIngredient-group"
-                    name="group"
-                    value={formik2.values.group}
-                    onChange={handleSelectGroup}
-                    variant="outlined"
-                    fullWidth>
-                    {ingredientGroups?.length > 0 &&
-                      ingredientGroups.map(el => (
-                        <MenuItem key={'group' + el.pk} value={el.pk}>
-                          {el.title}
-                        </MenuItem>
-                      ))}
-                  </Select>
-                </div>
-                <div className={classes.addIngredient__buttonContainer}>
-                  <button type="submit" disabled={!formik2.isValid} className={classes.addIngredient__button}>
-                    Create
-                  </button>
-                  {Object.values(formik2.errors)?.length > 0 && <p>{Object.values(formik2.errors)[0]}</p>}
-                </div>
-              </form>
-            </div>
-          </LayoutModal>
-        </Dialog>
       </div>
     );
   };
@@ -455,4 +289,4 @@ function AddIngredient(props) {
 
 export default connect(state => ({
   recipeUpload: state.recipeUpload
-}))(AddIngredient);
+}))(CreateNewIngredient);
