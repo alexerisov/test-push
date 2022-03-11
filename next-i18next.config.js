854a2017c71ee35502fac95300b1c5f0b0ef2d1d
Alexey Erisov <alexerisov@gmail.com>
2022-03-11 23:03:46 +0700

    upd: Add 'ru' and 'de' locales

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ let path = require('path');
 
 module.exports = {
   i18n: {
-    locales: ['en', 'nl'],
+    locales: ['en', 'nl', 'ru', 'de'],
     defaultLocale: 'nl',
     localeDetection: true,
     objectNotation: true
