50e04c8d3b6c979800ddd19e87af9c52583189d6
Alexey Erisov <alexerisov@gmail.com>
2022-04-01 16:13:17 +0700

    fix: Enable ingredients translations

--- src/pages/recipe/[id].ts
+++ src/pages/recipe/[id].ts
@@ -3,6 +3,7 @@ import Recipe from '@/api/Recipe.js';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { RootState } from '@/store/store';
 import { getSession } from 'next-auth/react';
+import * as Sentry from '@sentry/nextjs';
 
 // page component
 import { RecipePage } from '@/components/pages/recipe/RecipePage';
@@ -10,22 +11,20 @@ import http from '@/utils/http';
 import { LANGUAGES } from '@/utils/datasets';
 import { setBearer } from '@/utils/setBearer';
 
-export default connect((state: RootState) => ({
-  account: state.account
-}))(RecipePage);
+export default RecipePage;
 
 export async function getServerSideProps(context) {
   const id = context.params.id;
   const session = await getSession(context);
   setBearer(session?.jwt);
   let recipeResponse;
+
   try {
     if (session) {
-      recipeResponse = await Recipe.getRecipe(id, LANGUAGES[session?.user?.language]);
+      const recipeResponse = await Recipe.getRecipe(id, LANGUAGES[session?.user?.language]);
     } else {
       recipeResponse = await Recipe.getRecipe(id);
     }
-    const weekmenuResponse = await Recipe.getWeekmenu('');
     const topRatedResponse = await Recipe.getTopRatedMeals();
 
     return {
@@ -33,18 +32,16 @@ export async function getServerSideProps(context) {
         session,
         ...(await serverSideTranslations(context.locale, ['common', 'recipePage', 'recipeClassifications'])),
         recipe: recipeResponse.data,
-        weekmenu: weekmenuResponse.data,
         topRatedRecipes: topRatedResponse.data,
-        absolutePath: context.req.headers.host,
-        notFound: false
+        absolutePath: context.req.headers.host
       }
     };
   } catch (e) {
     console.error(e);
+    Sentry.captureException(e);
 
     return {
       props: {
-        ...(await serverSideTranslations(context.locale, ['common'])),
         notFound: true
       }
     };
